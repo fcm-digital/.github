836d6b80322ad2651d37a5d2730cdca20d5eeb20
David Pacheco <David.Pacheco@fcm.digital>
2023-02-15 09:48:25 +0100

    Group update-chart values into one action

---
+++ .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ "$DEPLOYMENT_TYPE" == "local" ]]; then
+    cd kube/values/$APP_NAME
+elif [[ "$DEPLOYMENT_TYPE" == "remote" ]]; then
+    cd helm-chart-template/$APP_NAME
+else
+    exit 1
+fi
+
+
+if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
+    echo "The current Branch Name is not allowed. It must NOT start with 'prod'"
+    exit 1
+fi
+
+if [[ "$ENV_TO_DEPLOY" == "master" && github.ref != 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref != 'refs/heads/main' ]]; then
+    echo "The current Branch Name is not allowed. It must NOT start with 'master' or 'main'"
+    exit 1
+fi
+
+
+if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]]; then
+    if [[ github.ref == 'refs/heads/master' ]] || [[ github.ref == 'refs/heads/main' ]]; then
+        for env_file in "values-"*; do
+            [[ -e "$env_file" ]] || break
+            if [[ $env_file != *"prod.yaml" ]]; then
+                if [[ "$DEPLOYMENT_TYPE" == "local" ]]; then
+                    sed -i '{n;s/current_tag:.*/current_tag: '$IMAGE_TAG'/;}' $env_file
+                elif [[ "$DEPLOYMENT_TYPE" == "remote" ]]; then
+                    cp -f ../../kube/values/$APP_NAME/$env_file $APP_NAME/$env_file
+                else
+                    exit 1
+                fi
+            fi
+        done
+    fi
+else
+    if [[ "$ENV_TO_DEPLOY" == "master" && github.ref == 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref == 'refs/heads/main' ]]; then
+        VALUES_FILE="values-prod.yaml"
+    else
+        VALUES_FILE="values-$ENV_TO_DEPLOY.yaml"
+    fi
+
+    if [[ "$DEPLOYMENT_TYPE" == "local" ]]; then
+        echo "OLD_IMAGE_TAG=$(cat $VALUES_FILE | grep current_tag: | cut -d ':' -f 2 | sed 's/ //g')" >> $GITHUB_ENV
+        sed -i '{n;s/current_tag:.*/current_tag: '$IMAGE_TAG'/;}' $VALUES_FILE
+    elif [[ "$DEPLOYMENT_TYPE" == "remote" ]]; then
+        cp -f ../../kube/values/$APP_NAME/$VALUES_FILE $APP_NAME/$VALUES_FILE
+    else
+        exit 1
+    fi
+fi
+
+git status
+# git config user.name github-actions
+# git config user.email github-actions@github.com
+# git pull
+# git add .
+# git commit -m "New Deployment in $APP_NAME - $IMAGE_TAG for $ENV_TO_DEPLOY"
+# git push
