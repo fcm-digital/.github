9d51630e5e2729db6685f71a902230d2742d7a68
David Pacheco <David.Pacheco@fcm.digital>
2023-02-15 11:21:19 +0100

    test update-chart-values workflow 4

--- .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
+++ .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
@@ -54,4 +54,9 @@ else
     fi
 fi
 
-cat $VALUES_FILE
+git config user.name github-actions
+git config user.email github-actions@github.com
+git pull
+git add .
+git commit -m "New Deployment in $APP_NAME - $IMAGE_TAG for $ENV_TO_DEPLOY"
+git push
