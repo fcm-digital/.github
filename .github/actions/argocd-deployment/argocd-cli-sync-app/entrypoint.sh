d29f0b04e9d05784dcd6d3f92f0411c7dac33fd0
David Pacheco <David.Pacheco@fcm.digital>
2023-02-23 10:30:05 +0100

    add region and environment as variables

--- .github/actions/argocd-deployment/argocd-cli-sync-app/entrypoint.sh
+++ .github/actions/argocd-deployment/argocd-cli-sync-app/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
 argocd_app_sync () {
-    argocd app sync $ARGOCD_APP_NAME \
+    argocd app sync $ARGOCD_FULL_APP_NAME \
         --server $ARGOCD_URL \
         --auth-token $ARGOCD_AUTH_TOKEN \
         --retry-limit 10 \
         --retry-backoff-duration 10s \
         --retry-backoff-factor 2
 }
+
+
+if [[ "$ARGOCD_CURRENT_ENV" == "master" && github.ref == 'refs/heads/master' ]] || [[ "$ARGOCD_CURRENT_ENV" == "main" && github.ref == 'refs/heads/main' ]]; then
+    ARGOCD_FULL_APP_NAME="$ARGOCD_APP_NAME-prod-$ARGOCD_APP_REGION"
+else
+    ARGOCD_FULL_APP_NAME="$ARGOCD_APP_NAME-$ARGOCD_CURRENT_ENV-stg-$ARGOCD_APP_REGION"
+fi
+
 
 ITER=1
 
