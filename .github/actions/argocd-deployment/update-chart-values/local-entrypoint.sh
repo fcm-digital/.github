189f18479d8d4d832e1bc28daf86e4ff45144f48
David Pacheco <David.Pacheco@fcm.digital>
2023-02-28 14:04:23 +0100

    split update-chart-values scripts into local and remote

---
+++ .github/actions/argocd-deployment/update-chart-values/local-entrypoint.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd kube/values/$APP_NAME
+
+
+# The branch name cannot start with 'prod'.
+if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
+    echo "The current Branch Name is not allowed. It must NOT start with 'prod'"
+    exit 1
+fi
+
+# The branch name can only start with 'master' or 'main' if the branch is MASTER/MAIN ref.
+if [[ "$ENV_TO_DEPLOY" == "master" && github.ref != 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref != 'refs/heads/main' ]]; then
+    echo "The current Branch Name is not allowed. It must NOT start with 'master' or 'main'"
+    exit 1
+fi
+
+# Iter over all values-*.yaml files in order to update their current_tag value with the new deployment tag.
+if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]] && [[ github.ref == 'refs/heads/master' || github.ref == 'refs/heads/main' ]]; then
+    for env_file in "values-"*; do
+        [[ -e "$env_file" ]] || break
+        if [[ $env_file != *"prod.yaml" ]]; then # Only in staging environments.
+            sed -i '{n;s/current_tag:.*/current_tag: '$IMAGE_TAG'/;}' $env_file
+        fi
+    done
+fi
+
+if [[ "$ENV_TO_DEPLOY" == "master" && github.ref == 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref == 'refs/heads/main' ]]; then
+    VALUES_FILE="values-prod.yaml"
+else
+    VALUES_FILE="values-$ENV_TO_DEPLOY.yaml"
+fi
+
+# Store the current_tag value before the deployment for rollout undo (just in case).
+echo "OLD_IMAGE_TAG=$(cat $VALUES_FILE | grep current_tag: | cut -d ':' -f 2 | sed 's/ //g')" >> $GITHUB_ENV
+# Replace current_tag value in the current environment
+sed -i '{n;s/current_tag:.*/current_tag: '$IMAGE_TAG'/;}' $VALUES_FILE
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git pull
+git add .
+if $ROLLOUT; then
+    git commit -m "Rollout Undo in $APP_NAME - $IMAGE_TAG for $ENV_TO_DEPLOY"
+else
+    git commit -m "New Deployment in $APP_NAME - $IMAGE_TAG for $ENV_TO_DEPLOY"
+fi
+git push
---
+++ .github/actions/argocd-deployment/update-chart-values/remote-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd helm-chart-template/$APP_NAME
+
+
+# The branch name cannot start with 'prod'
+if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
+    echo "The current Branch Name is not allowed. It must NOT start with 'prod'"
+    exit 1
+fi
+
+# The branch name can only start with 'master' or 'main' if the branch is MASTER/MAIN ref.
+if [[ "$ENV_TO_DEPLOY" == "master" && github.ref != 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref != 'refs/heads/main' ]]; then
+    echo "The current Branch Name is not allowed. It must NOT start with 'master' or 'main'"
+    exit 1
+fi
+
+# Iter over all values-*.yaml files in order to sync thier content with the local values.
+if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]] && [[ github.ref == 'refs/heads/master' || github.ref == 'refs/heads/main' ]]; then
+    for env_file in "values-"*; do
+        [[ -e "$env_file" ]] || break
+        if [[ $env_file != *"prod.yaml" ]]; then # Only in staging environments.
+            cp -f ../../kube/values/$APP_NAME/$env_file $env_file
+        fi
+    done
+fi
+
+if [[ "$ENV_TO_DEPLOY" == "master" && github.ref == 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref == 'refs/heads/main' ]]; then
+    cp -f ../../kube/values/$APP_NAME/values.yaml values.yaml
+    VALUES_FILE="values-prod.yaml"
+else
+    VALUES_FILE="values-$ENV_TO_DEPLOY.yaml"
+fi
+
+# Sync the remote value file with the content with the local values
+cp -f ../../kube/values/$APP_NAME/$VALUES_FILE $VALUES_FILE
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git pull
+git add .
+if $ROLLOUT; then
+    git commit -m "Rollout Undo in $APP_NAME - $IMAGE_TAG for $ENV_TO_DEPLOY"
+else
+    git commit -m "New Deployment in $APP_NAME - $IMAGE_TAG for $ENV_TO_DEPLOY"
+fi
+git push
