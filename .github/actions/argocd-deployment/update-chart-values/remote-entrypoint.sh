4b1070fd110e08a6778b9dd10cdabd2f396dabb1
David Pacheco <David.Pacheco@fcm.digital>
2023-05-04 17:11:49 +0200

    Update remote-entrypoint.sh

--- .github/actions/argocd-deployment/update-chart-values/remote-entrypoint.sh
+++ .github/actions/argocd-deployment/update-chart-values/remote-entrypoint.sh
@@ -19,23 +19,23 @@ fi
 
 # Iter over all values-*.yaml files in order to sync thier content with the local values.
 if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]] && [[ github.ref == 'refs/heads/master' || github.ref == 'refs/heads/main' ]]; then
-    for env_file in "values-"*; do
+    for env_file in "values-stg-"*; do
         [[ -e "$env_file" ]] || break
         if [[ $env_file != *"prod.yaml" ]]; then # Only in staging environments.
             cp -f ../../kube/values/$APP_NAME/$env_file $env_file
         fi
     done
-fi
-
-if [[ "$ENV_TO_DEPLOY" == "master" && github.ref == 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref == 'refs/heads/main' ]]; then
-    cp -f ../../kube/values/$APP_NAME/values.yaml values.yaml
-    VALUES_FILE="values-prod.yaml"
 else
-    VALUES_FILE="values-$ENV_TO_DEPLOY.yaml"
+    if [[ "$ENV_TO_DEPLOY" == "master" && github.ref == 'refs/heads/master' ]] || [[ "$ENV_TO_DEPLOY" == "main" && github.ref == 'refs/heads/main' ]]; then
+        cp -f ../../kube/values/$APP_NAME/values.yaml values.yaml
+        VALUES_FILE="values-prod.yaml"
+    else
+        VALUES_FILE="values-stg-$ENV_TO_DEPLOY.yaml"
+    fi
+    # Sync the remote value file with the content with the local values
+    cp -f ../../kube/values/$APP_NAME/$VALUES_FILE $VALUES_FILE
 fi
 
-# Sync the remote value file with the content with the local values
-cp -f ../../kube/values/$APP_NAME/$VALUES_FILE $VALUES_FILE
 
 git config user.name github-actions
 git config user.email github-actions@github.com
