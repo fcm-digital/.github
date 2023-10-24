20b94960feb38f562fc3e9655c61c1d8e5b6b601
David Pacheco <David.Pacheco@fcm.digital>
2023-10-24 08:30:59 +0200

    Add deployed_at env for manual deployment

--- .github/actions/argocd-deployment/update-imagetag-chart-values/entrypoint.sh
+++ .github/actions/argocd-deployment/update-imagetag-chart-values/entrypoint.sh
@@ -2,12 +2,23 @@
 
 set -euo pipefail
 
+if [[ "$UPDATE_DEPLOYED_AT" = true ]]; then
+    echo "Updating 'DEPLOYED_AT' env variable at runtime."
+    DEPLOYED_AT=$(date -u +"%FT%TZ")
+fi
+
 if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
     cd helm-chart-$APP_NAME-values-prod/
     sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./prod/values-prod-tag.yaml"
+    if [ ! -z ${DEPLOYED_AT+x} ]; then
+        sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./prod/values-stg.yaml"
+    fi
 else
     cd helm-chart-$APP_NAME-values-staging/
     sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$ENV_TO_DEPLOY/values-stg-tag.yaml"
+    if [ ! -z ${DEPLOYED_AT+x} ]; then
+        sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./staging/$ENV_TO_DEPLOY/values-stg.yaml"
+    fi
 fi
 
 git config user.name github-actions
