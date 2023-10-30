5c4aa76fe0187c4375b91ca14fa96610d9970771
dapama <45515268+dapama@users.noreply.github.com>
2023-10-30 10:41:55 +0100

    Allow Manual Deployments for ArgoCD Apps (#9)
    * Remove input types and test outputs locally
    
    * Add if statement to test outputs
    
    * Test error case
    
    * Improve if statement for checkout
    
    * Remove unexpected symbol: '\'
    
    * Init manual-checkout workflow
    
    * Fix typo in manual-checkout
    
    * Add id to initial step
    
    * Upgrade update-chart-values action to allow staging merge filter
    
    * Fix typo
    
    * Improve env_to_deploy variable definition
    
    * Test echo line
    
    * Fix type within if statement
    
    * Update comments
    
    * Remove unnecessary comments

--- .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
+++ .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
@@ -2,19 +2,25 @@
 
 set -euo pipefail
 
-
-# The branch name cannot start with 'prod'.
-if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
-    echo "The current Branch Name is not allowed. It must NOT start with 'prod'"
-    exit 1
-fi
-
 # The branch name can only start with 'master' or 'main' if the branch is MASTER/MAIN ref.
-if [[ "$ENV_TO_DEPLOY" == "master" && "$BRANCH_NAME" != "master" ]] || [[ "$ENV_TO_DEPLOY" == "main" && "$BRANCH_NAME" != "main" ]]; then
-    echo "The current Branch Name is not allowed. It must NOT start with 'master' or 'main'"
+if [[ "$ENV_TO_DEPLOY" == "prod" ]] && 
+   [[ "$BRANCH_NAME" != "master" || "$BRANCH_NAME" != "main" ]]; then
+    echo "The Environment to Deploy cannot be 'prod' if the branches are not 'master' or 'main'."
     exit 1
 fi
 
+if [[ "$ENV_TO_DEPLOY" != "prod" && "$ENV_TO_DEPLOY" != "ALL_ENV" ]] &&
+   [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
+    echo "The Environment to Deploy must be 'prod' or 'ALL_ENV' if the branches are 'master' or 'main'."
+    exit 1
+fi
+
+if [[ "$ENV_TO_DEPLOY" == "master" || "$ENV_TO_DEPLOY" == "main" ]]; then
+    echo "The Environment to Deploy cannot be 'master' or 'main'."
+    exit 1
+fi
+
+# 'BFF' and 'BIGBROTHER' environment variable -> Must be updated in each deploy.
 if [[ "$UPDATE_DEPLOYED_AT" = true ]]; then
     echo "Updating 'DEPLOYED_AT' env variable at runtime."
     DEPLOYED_AT=$(date -u +"%FT%TZ")
@@ -24,15 +30,25 @@ fi
 if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]] && [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
     cd helm-chart-$APP_NAME-values-staging/
     for env_path in $(ls -d -- ./../kube/values/$APP_NAME/staging/*/); do
-        sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$(basename "${env_path%/}")/values-stg-tag.yaml"
-        if [ ! -z ${DEPLOYED_AT+x} ]; then
-            sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./../kube/values/$APP_NAME/staging/$(basename "${env_path%/}")/values-stg.yaml"
+        # Get the source of the 'currentTag' environment
+        export CURRENT_ENV=$(basename "${env_path%/}")
+        export CURRENT_IMAGE_TAG=$(cat "./staging/$CURRENT_ENV/values-stg-tag.yaml" | grep currentTag: | cut -d ':' -f 2 | sed 's/ //g')
+        export CURRENT_IMAGE_TAG_ENV=$(cut -d '-' -f 1 <<< $( echo $CURRENT_IMAGE_TAG ))
+
+        # Check if the currentTag is an old 'master' image -> Then, sync the values.
+        # Sandbox will always be synced when a new 'master' image is deployed.
+        if [[ "$CURRENT_IMAGE_TAG_ENV" == "master" || "$CURRENT_IMAGE_TAG_ENV" == "main" || "$CURRENT_ENV" == "sandbox" ]]; then
+            sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$CURRENT_ENV/values-stg-tag.yaml"
+            if [ ! -z ${DEPLOYED_AT+x} ]; then
+                sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml"
+            fi
+            cp -f "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml" "./staging/$CURRENT_ENV/values-stg.yaml"
         fi
-        cp -f "./../kube/values/$APP_NAME/staging/$(basename "${env_path%/}")/values-stg.yaml" "./staging/$(basename "${env_path%/}")/values-stg.yaml"
     done
+    # The values-stg.yaml will always be synced when a Pull Request is closed.
     cp -f "./../kube/values/$APP_NAME/staging/values-stg.yaml" "./staging/values-stg.yaml"
 
-elif [[ "$ENV_TO_DEPLOY" == "master" && "$BRANCH_NAME" == "master" ]] || [[ "$ENV_TO_DEPLOY" == "main" && "$BRANCH_NAME" == "main" ]]; then
+elif [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
     cd helm-chart-$APP_NAME-values-prod/
     # Store the currentTag value before the deployment for rollout undo (just in case).
     echo "OLD_IMAGE_TAG=$(cat "./prod/values-prod-tag.yaml" | grep currentTag: | cut -d ':' -f 2 | sed 's/ //g')" >> $GITHUB_ENV
