02951cd0a07e6d9eaafa485ed96c562a5cb9a664
David Pacheco <David.Pacheco@fcm.digital>
2023-11-13 08:42:04 +0100

    Update manual-deploy action to include ALL_ENV deployment

--- .github/actions/argocd-deployment/update-chart-values-manual-deploy/entrypoint.sh
+++ .github/actions/argocd-deployment/update-chart-values-manual-deploy/entrypoint.sh
@@ -13,6 +13,23 @@ if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
     if [ ! -z ${DEPLOYED_AT+x} ]; then
         sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./prod/values-prod.yaml"
     fi
+
+elif [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]]; then
+    cd helm-chart-$APP_NAME-values-staging/
+    for env_path in $(ls -d -- ./../kube/values/$APP_NAME/staging/*/); do
+        # Get the source of the 'currentTag' environment
+        export CURRENT_ENV=$(basename "${env_path%/}")
+        export CURRENT_IMAGE_TAG=$(cat "./staging/$CURRENT_ENV/values-stg-tag.yaml" | grep currentTag: | cut -d ':' -f 2 | sed 's/ //g')
+        export CURRENT_IMAGE_TAG_ENV=$(cut -d '-' -f 1 <<< $( echo $CURRENT_IMAGE_TAG ))
+
+        if [[ "$CURRENT_IMAGE_TAG_ENV" == "master" || "$CURRENT_IMAGE_TAG_ENV" == "main" || "$CURRENT_ENV" == "sandbox" ]]; then
+            sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$CURRENT_ENV/values-stg-tag.yaml"
+            if [ ! -z ${DEPLOYED_AT+x} ]; then
+                sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml"
+            fi
+            cp -f "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml" "./staging/$CURRENT_ENV/values-stg.yaml"
+        fi
+    done
 else
     cd helm-chart-$APP_NAME-values-staging/
     sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$ENV_TO_DEPLOY/values-stg-tag.yaml"
