79017cef57ceb57b100aac7a580ad83a0a757867
David Pacheco <David.Pacheco@fcm.digital>
2024-04-01 13:28:14 +0200

    Update update-chart-values/entrypoint.sh

--- .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
+++ .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
@@ -9,18 +9,12 @@ if [[ "$ENV_TO_DEPLOY" == "prod" ]] &&
     exit 1
 fi
 
-# if [[ "$ENV_TO_DEPLOY" != "prod" && "$ENV_TO_DEPLOY" != "ALL_ENV" ]] &&
-#    [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
-#     echo "The Environment to Deploy must be 'prod' or 'ALL_ENV' if the branches are 'master' or 'main'."
-#     exit 1
-# fi
-
 if [[ "$ENV_TO_DEPLOY" == "master" || "$ENV_TO_DEPLOY" == "main" ]]; then
     echo "The Environment to Deploy cannot be 'master' or 'main'."
     exit 1
 fi
 
-# 'BFF' and 'BIGBROTHER' environment variable -> Must be updated in each deploy.
+# Must be updated in each deploy.
 if [[ "$UPDATE_DEPLOYED_AT" = true ]]; then
     echo "Updating 'DEPLOYED_AT' env variable at runtime."
     DEPLOYED_AT=$(date -u +"%FT%TZ")
@@ -34,15 +28,20 @@ if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]] && [[ "$BRANCH_NAME" == "master" || "$BRA
         export CURRENT_ENV=$(basename "${env_path%/}")
         export CURRENT_IMAGE_TAG=$(cat "./staging/$CURRENT_ENV/values-stg-tag.yaml" | grep currentTag: | cut -d ':' -f 2 | sed 's/ //g')
         export CURRENT_IMAGE_TAG_ENV=$(cut -d '-' -f 1 <<< $( echo $CURRENT_IMAGE_TAG ))
+        export CURRENT_SOURCE_FILE=$(echo "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml")
 
-        # Check if the currentTag is an old 'master' image -> Then, sync the values.
-        # Sandbox will always be synced when a new 'master' image is deployed.
-        if [[ "$CURRENT_IMAGE_TAG_ENV" == "master" || "$CURRENT_IMAGE_TAG_ENV" == "main" || "$CURRENT_ENV" == "sandbox" ]]; then
-            sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$CURRENT_ENV/values-stg-tag.yaml"
-            if [ ! -z ${DEPLOYED_AT+x} ]; then
-                sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml"
+        if [[ -e $CURRENT_SOURCE_FILE ]]; then
+            # Check if the currentTag is an old 'master' image -> Then, sync the values.
+            # Sandbox will always be synced when a new 'master' image is deployed.
+            if [[ "$CURRENT_IMAGE_TAG_ENV" == "master" || "$CURRENT_IMAGE_TAG_ENV" == "main" || "$CURRENT_ENV" == "sandbox" ]]; then
+                sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$CURRENT_ENV/values-stg-tag.yaml"
+                if [ ! -z ${DEPLOYED_AT+x} ]; then
+                    sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" $CURRENT_SOURCE_FILE
+                fi
+                cp -f $CURRENT_SOURCE_FILE "./staging/$CURRENT_ENV/values-stg.yaml"
             fi
-            cp -f "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml" "./staging/$CURRENT_ENV/values-stg.yaml"
+        else
+            echo "$CURRENT_ENV not found in local code repository, but existing in helm-chart-$APP_NAME-values/staging repository."
         fi
     done
     # The values-stg.yaml will always be synced when a Pull Request is closed.
