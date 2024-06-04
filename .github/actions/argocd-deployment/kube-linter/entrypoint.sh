d9744dd38fad71e39e6f71f1dbee710ef870a0c0
David Pacheco <David.Pacheco@fcm.digital>
2024-06-04 14:23:44 +0200

    Update entrypoint.sh

--- .github/actions/argocd-deployment/kube-linter/entrypoint.sh
+++ .github/actions/argocd-deployment/kube-linter/entrypoint.sh
@@ -2,27 +2,22 @@
 
 set -euo pipefail
 
+cd helm-chart-template
 
 HELM_VALUES=""
 
 if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
-    cd kube/values/$APP_NAME/prod
-
     while IFS= read -r -d '' file; do
         HELM_VALUES+="-f \"$file\" "
-    done < <(find '.' -type f -name '*.yaml' -print0)
+    done < <(find ../kube/values/$APP_NAME/prod -type f -name '*.yaml' -print0)
 else
-    cd kube/values/$APP_NAME/staging
+    while IFS= read -r -d '' file; do
+        HELM_VALUES+="-f \"$file\" "
+    done < <(find ../kube/values/$APP_NAME/staging -maxdepth 1 -type f -name '*.yaml' -print0)
 
     while IFS= read -r -d '' file; do
         HELM_VALUES+="-f \"$file\" "
-    done < <(find '.' -maxdepth 1 -type f -name '*.yaml' -print0)
-
-    cd $ENV_TO_DEPLOY
-
-    while IFS= read -r -d '' file; do
-        HELM_VALUES+="-f \"$file\" "
-    done < <(find '.' -type f -name '*.yaml' -print0)
+    done < <(find ../kube/values/$APP_NAME/staging/$ENV_TO_DEPLOY -type f -name '*.yaml' -print0)
 fi
 
 helm template . --name-template=$APP_NAME --namespace=$ENV_TO_DEPLOY \
