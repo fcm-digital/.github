b147a86b957c23769ea1dd8e3c5cb7211d12fe6c
dapama <45515268+dapama@users.noreply.github.com>
2024-06-18 13:04:31 +0200

    Improve Kube Linter - Add NO_SYNC option to update-chart-values action (#15)
    * Update action.yaml
    
    * Update action.yaml
    
    * Update action.yaml
    
    * Update action.yaml
    
    * move kube-linter action to entrypoint.sh
    
    * Update action.yaml and entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update action.yaml and entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update action.yaml and entrypoint.sh
    
    * Update action.yaml
    
    * Update entrypoint.sh
    
    * Update action.yaml
    
    * Include NO_SYNC option for update-chart-values action
    
    * Add dependabot constraint
    
    * Update action.yaml
    
    * Update action.yaml
    
    * Update action.yaml
    
    * Update action.yaml

---
+++ .github/actions/argocd-deployment/kube-linter/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd helm-chart-template
+
+HELM_VALUES=""
+
+if [[ "$ENV_TO_DEPLOY" == "prod" ]]; then
+    while IFS= read -r -d '' file; do
+        HELM_VALUES+="-f \"$file\" "
+    done < <(find ../kube/values/$APP_NAME/prod -type f -name '*.yaml' -print0)
+else
+    while IFS= read -r -d '' file; do
+        HELM_VALUES+="-f \"$file\" "
+    done < <(find ../kube/values/$APP_NAME/staging -maxdepth 1 -type f -name '*.yaml' -print0)
+
+    while IFS= read -r -d '' file; do
+        HELM_VALUES+="-f \"$file\" "
+    done < <(find ../kube/values/$APP_NAME/staging/$ENV_TO_DEPLOY -type f -name '*.yaml' -print0)
+fi
+
+helm template . --name-template=$APP_NAME --namespace=$ENV_TO_DEPLOY \
+    --set currentTag=$IMAGE_TAG $HELM_VALUES > output-template.yaml
+
+kube-linter lint output-template.yaml --exclude non-existent-service-account $EXCLUDE_RULES
