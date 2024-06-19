6f09c1facd01c936dec1d853b8856960d5a43518
David Pacheco <David.Pacheco@fcm.digital>
2024-06-19 11:46:02 +0200

    Update sync_app_entrypoint.sh

--- .github/actions/argocd-deployment/argocd-cli-sync-app/sync_app_entrypoint.sh
+++ .github/actions/argocd-deployment/argocd-cli-sync-app/sync_app_entrypoint.sh
@@ -10,6 +10,13 @@ argocd_app_sync () {
         --retry-backoff-factor 2
 }
 
+argocd_app_wait () {
+    argocd app wait $ARGOCD_FULL_APP_NAME \
+        --server $ARGOCD_URL \
+        --auth-token $ARGOCD_AUTH_TOKEN \
+        --sync
+}
+
 if [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
     ARGOCD_FULL_APP_NAME="$APP_NAME-pro-$APP_REGION"
 else
@@ -20,6 +27,18 @@ fi
 ITER=1
 
 until argocd_app_sync </dev/null
+do
+    if [ $ITER -eq 3 ]; then
+        exit 1
+    fi
+
+    sleep $((10 * $ITER))s
+    ITER=$(($ITER + 1))
+done
+
+ITER=1
+
+until argocd_app_wait </dev/null
 do
     if [ $ITER -eq 3 ]; then
         exit 1
