b112c2762e4fb54189a9233c3f7ae46746baa145
David Pacheco <David.Pacheco@fcm.digital>
2024-09-02 13:00:31 +0200

    Update get_resources_filter_entrypoint.sh

--- .github/actions/argocd-deployment/argocd-cli-sync-app/get_resources_filter_entrypoint.sh
+++ .github/actions/argocd-deployment/argocd-cli-sync-app/get_resources_filter_entrypoint.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
 argocd_app_get_resources () {
-
     if [ -n "$RESOURCES_FILTER" ]; then
-        FILTER_PARAMS=$( echo $RESOURCES_FILTER | awk '{for(i=1;i<=NF;i++) printf "-e %s ", $i}' )
+        local filter_params=$( echo $RESOURCES_FILTER | awk '{for(i=1;i<=NF;i++) printf "-e %s ", $i}' )
         local resources_to_sync=$(argocd app resources $ARGOCD_FULL_APP_NAME \
             --server $ARGOCD_URL \
-            --auth-token $ARGOCD_AUTH_TOKEN | awk '$NF == "No" {print $(NF-3) "_" $(NF-1)}' | grep $FILTER_PARAMS )
+            --auth-token $ARGOCD_AUTH_TOKEN | awk '$NF == "No" {print $(NF-3) "_" $(NF-1)}' | grep $filter_params )
 
     elif [ -n "$IGNORE_RESOURCES_FILTER" ]; then
-        FILTER_PARAMS=$( echo $IGNORE_RESOURCES_FILTER | awk '{for(i=1;i<=NF;i++) printf "-e %s ", $i}' )
+        local filter_params=$( echo $IGNORE_RESOURCES_FILTER | awk '{for(i=1;i<=NF;i++) printf "-e %s ", $i}' )
         local resources_to_sync=$(argocd app resources $ARGOCD_FULL_APP_NAME \
             --server $ARGOCD_URL \
-            --auth-token $ARGOCD_AUTH_TOKEN | awk '$NF == "No" {print $(NF-3) "_" $(NF-1)}' | grep -v $FILTER_PARAMS )
+            --auth-token $ARGOCD_AUTH_TOKEN | awk '$NF == "No" {print $(NF-3) "_" $(NF-1)}' | grep -v $filter_params )
     else
         echo "resources_filter=" >> $GITHUB_OUTPUT
     fi
 
-    if [ -z "$resources_to_sync" ]; then
+    if [ -n "$resources_to_sync" ]; then
         echo "resources_filter=$( echo "$resources_to_sync" | sed 's/_/:/g' | awk '{for(i=1;i<=NF;i++) printf "--resource '"'*:%s'"' ", $i}' )" >> $GITHUB_OUTPUT
     fi
 }
