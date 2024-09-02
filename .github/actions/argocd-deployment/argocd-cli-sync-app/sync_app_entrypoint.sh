0805873c50eb95334a8fecce995fbe71ef04757f
David Pacheco <David.Pacheco@fcm.digital>
2024-09-02 13:50:37 +0200

    another test

--- .github/actions/argocd-deployment/argocd-cli-sync-app/sync_app_entrypoint.sh
+++ .github/actions/argocd-deployment/argocd-cli-sync-app/sync_app_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 argocd_app_sync () {
+    echo "argocd app sync $ARGOCD_FULL_APP_NAME --server $ARGOCD_URL --auth-token $ARGOCD_AUTH_TOKEN --prune --retry-limit 2 --retry-backoff-duration 5s --retry-backoff-factor 2 $RESOURCES $LABELS"
     argocd app sync $ARGOCD_FULL_APP_NAME \
         --server $ARGOCD_URL \
         --auth-token $ARGOCD_AUTH_TOKEN \
