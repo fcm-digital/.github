3c9fedc664743e5f36f2955ec4cf0bbac7e16e6e
David Pacheco <David.Pacheco@fcm.digital>
2025-01-20 14:38:07 +0100

    Update entrypoint.sh

--- .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
+++ .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
@@ -5,7 +5,22 @@ set -euox pipefail
 if [[ -f $ARGO_WORKFLOW_FILE ]]; then
     echo "Submitting Argo Workflow file $ARGO_WORKFLOW_FILE"
     argo submit $ARGO_WORKFLOW_FILE
-    argo watch @latest -n $ARGO_NAMESPACE
+    argo wait @latest -n $ARGO_NAMESPACE
+    if [[ $? -eq 0 ]]; then
+        echo "Argo Workflow $ARGO_WORKFLOW_FILE submitted successfully"
+        ARGO_OUTPUT_STATUS=$(argo get @latest -n $ARGO_NAMESPACE | grep "Status:" | awk '{print $2}')
+        if [[ $ARGO_OUTPUT_STATUS == "Succeeded" ]]; then
+            echo "Argo Workflow $ARGO_WORKFLOW_FILE completed successfully"
+            exit 0
+        else
+            echo "Argo Workflow $ARGO_WORKFLOW_FILE failed"
+            argo get @latest -n $ARGO_NAMESPACE
+            exit 1
+        fi
+    else
+        echo "Argo Workflow $ARGO_WORKFLOW_FILE failed"
+        exit 1
+    fi
 else
     echo "File $ARGO_WORKFLOW_FILE not found"
     exit 1
