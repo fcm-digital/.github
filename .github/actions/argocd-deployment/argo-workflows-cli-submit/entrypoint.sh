aa0560423bf7f9bd56fe379762d541343a3e1a8e
David Pacheco <David.Pacheco@fcm.digital>
2025-01-20 13:27:01 +0100

    Update entrypoint.sh 5

--- .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
+++ .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
@@ -5,6 +5,7 @@ set -euox pipefail
 if [[ -f $ARGO_WORKFLOW_FILE ]]; then
     echo "Submitting Argo Workflow file $ARGO_WORKFLOW_FILE"
     argo submit $ARGO_WORKFLOW_FILE
+    sleep 5s
     argo list
 else
     echo "File $ARGO_WORKFLOW_FILE not found"
