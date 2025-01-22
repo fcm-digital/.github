54794d91c48d27d407ff682f5534dba4ddb81a83
David Pacheco <David.Pacheco@fcm.digital>
2025-01-22 10:47:54 +0100

    update argo workflows to be able to work with a list on staging envs

--- .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
+++ .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
@@ -2,17 +2,22 @@
 
 set -euo pipefail
 
-if [[ -f $ARGO_WORKFLOW_FILE ]]; then
-    echo "Submitting Argo Workflow file $ARGO_WORKFLOW_FILE"
-    argo submit $ARGO_WORKFLOW_FILE
-    if [[ $? -eq 0 ]]; then
-        argo wait @latest -n $ARGO_NAMESPACE
+WORKFLOWS_PATH="./helm-chart-template"
+
+for env in $(echo $ENVIRONMENTS | tr ',' '\n'); do
+    ARGO_WORKFLOW_FILE="$WORKFLOWS_PATH/$ARGO_WORKFLOW_FILE-$env.yaml"
+    if [[ -f $WORKFLOW_FILE ]]; then
+        echo "Submitting Argo Workflow file $ARGO_WORKFLOW_FILE"
+        argo submit $ARGO_WORKFLOW_FILE
         if [[ $? -eq 0 ]]; then
-            echo "Argo Workflow $ARGO_WORKFLOW_FILE submitted successfully"
-            argo get @latest -n $ARGO_NAMESPACE
+            argo wait @latest -n $env
+            if [[ $? -eq 0 ]]; then
+                echo "Argo Workflow $ARGO_WORKFLOW_FILE submitted successfully"
+                argo get @latest -n $env
+            fi
         fi
+    else
+        echo "File $ARGO_WORKFLOW_FILE not found"
+        exit 1
     fi
-else
-    echo "File $ARGO_WORKFLOW_FILE not found"
-    exit 1
-fi
+done
--- .github/actions/argocd-deployment/argo-workflows-helm-template/entrypoint.sh
+++ .github/actions/argocd-deployment/argo-workflows-helm-template/entrypoint.sh
@@ -2,57 +2,55 @@
 
 set -euo pipefail
 
-if [ $ENVIRONMENT == "prod" ]; then
-    VALUES_DIR="./helm-chart-values/prod"
-else
-    VALUES_DIR="./helm-chart-values/staging"
-fi
+for env in $(echo $ENVIRONMENTS | tr ',' '\n'); do
 
-VALUES_GLOBAL_FILE=$(find $VALUES_DIR/ -maxdepth 1 -iname "*.yaml*")
-if [ ! -z "${VALUES_GLOBAL_FILE}" ]; then
-    for value in $VALUES_GLOBAL_FILE; do
-        VALUES+=$(echo "-f .$value ")
-    done
-fi
-
-VALUES_LOCAL_FILE=$(find "$VALUES_DIR/$ENVIRONMENT/" -iname "*.yaml*")
-if [ ! -z "${VALUES_LOCAL_FILE}" ]; then
-    for value in $VALUES_LOCAL_FILE; do
-        VALUES+=$(echo "-f .$value ")
-    done
-fi
-
-cd ./helm-chart-template
-helm template . -s templates/job.yaml --name-template=$APP_NAME --namespace=$ENVIRONMENT $VALUES --set currentTag=$IMAGE_TAG > "jobs-$ENVIRONMENT.yaml"
-
-
-delimiter="---"
-# delimiter="# Source: fcm-platform-helm-chart/templates/job.yaml"
-# sed -i 's/---//g' ./kube/values/itinerary-core/jobs.yaml
-
-csplit -zs --suppress-matched "jobs-$ENVIRONMENT.yaml" /"$delimiter"/ '{*}'
-
-for file in xx*; do
-    file_name=$(< $file grep 'type: ' --max-count=1 | awk '{ print $2 }')
-    echo $file_name
-    if [ -z "$file_name" ]; then
-        rm "$file"
+    if [ $env == "prod" ]; then
+        VALUES_DIR="./helm-chart-values/prod"
     else
-        mv "$file" "${file_name}.yaml"
+        VALUES_DIR="./helm-chart-values/staging"
     fi
-done
 
-
-helm template . -s templates/argo-workflows-orchestration.yaml --name-template=$APP_NAME --namespace=$ENVIRONMENT $VALUES --set currentTag=$IMAGE_TAG > argo-workflows.yaml
-
-csplit -zs --suppress-matched argo-workflows.yaml /"$delimiter"/ '{*}'
-
-for file in xx*; do
-    workflow=$(< $file grep "app.kubernetes.io/name: " --max-count=1 | awk '{ print $2 }')
-    echo $workflow
-    if [[ "$workflow" == "$WORKFLOW_NAME" ]]; then
-        mv "$file" "${WORKFLOW_NAME}.yaml"
-    else
-        rm "$file"
+    VALUES_GLOBAL_FILE=$(find $VALUES_DIR/ -maxdepth 1 -iname "*.yaml*")
+    if [ ! -z "${VALUES_GLOBAL_FILE}" ]; then
+        for value in $VALUES_GLOBAL_FILE; do
+            VALUES+=$(echo "-f .$value ")
+        done
     fi
+
+    VALUES_LOCAL_FILE=$(find "$VALUES_DIR/$env/" -iname "*.yaml*")
+    if [ ! -z "${VALUES_LOCAL_FILE}" ]; then
+        for value in $VALUES_LOCAL_FILE; do
+            VALUES+=$(echo "-f .$value ")
+        done
+    fi
+
+    cd ./helm-chart-template
+    helm template . -s templates/job.yaml --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "jobs-$env.yaml"
+
+    delimiter="---"
+    csplit -zs --suppress-matched "jobs-$env.yaml" /"$delimiter"/ '{*}'
+
+    for file in xx*; do
+        file_name=$(< $file grep 'type: ' --max-count=1 | awk '{ print $2 }')
+        echo $file_name
+        if [ -z "$file_name" ]; then
+            rm "$file"
+        else
+            mv "$file" "${file_name}-${env}.yaml"
+        fi
+    done
+
+    helm template . -s templates/argo-workflows-orchestration.yaml --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > argo-workflows-$env.yaml
+
+    csplit -zs --suppress-matched argo-workflows-$env.yaml /"$delimiter"/ '{*}'
+
+    for file in xx*; do
+        workflow=$(< $file grep "app.kubernetes.io/name: " --max-count=1 | awk '{ print $2 }')
+        if [[ "$workflow" == "$WORKFLOW_NAME" ]]; then
+            mv "$file" "${WORKFLOW_NAME}-${env}.yaml"
+        else
+            rm "$file"
+        fi
+    done
+
 done
