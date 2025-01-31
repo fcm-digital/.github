f4d19cd7f42aa67592aa1b25cca55e99ead8d89d
David Pacheco <David.Pacheco@fcm.digital>
2025-01-31 11:00:31 +0100

    remove alternative from MIMEMultipart

--- .github/actions/send-smtp-email/main.py
+++ .github/actions/send-smtp-email/main.py
@@ -28,7 +28,7 @@ def send_mail(smtp_enable_tls: bool, smtp_server_address: str, smtp_server_port:
     email_attachments (str): Email attachments.
   """
 
-  msg = MIMEMultipart("alternative")
+  msg = MIMEMultipart()
   recipients = email_cc.split(",") + email_bcc.split(",") + [email_to]
   msg['From'] = email_from
   msg['To'] = email_to
@@ -48,7 +48,7 @@ def send_mail(smtp_enable_tls: bool, smtp_server_address: str, smtp_server_port:
     html=f"""
     <html>
       <body>
-        <a href="{email_body_hyperlink}">{email_body_hyperlink_msg}</a>.
+        <a href="{email_body_hyperlink}">{email_body_hyperlink_msg}</a>
       </body>
     </html>
     """
