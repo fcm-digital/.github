5ca045faf99f9d8b6a0fef73e396873f88cf0774
dapama <45515268+dapama@users.noreply.github.com>
2025-03-12 07:52:16 +0100

    Update Release options for PROD update-chart-values [v2.2.0] (#24)
    * include release option in update-chart-values
    
    * update checkout-repositories-manual-deploy to include image_tag as output
    
    * improve image_tag creation
    
    * fix if clause to control prod deploys
    
    * add RELEASE_VERSION to the commit message

--- .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
+++ .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
@@ -18,8 +18,11 @@ add_synced_staging_envs() {
 # The branch name can only start with 'master' or 'main' if the branch is MASTER/MAIN ref.
 if [[ "$ENV_TO_DEPLOY" == "prod" ]] && 
    [[ "$BRANCH_NAME" != "master" && "$BRANCH_NAME" != "main" ]]; then
-    echo "The Environment to Deploy cannot be 'prod' if the branches are not 'master' or 'main'."
-    exit 1
+
+    if [[ -z "$RELEASE_VERSION" ]]; then
+        echo "The Environment to Deploy cannot be 'prod' if the branches are not 'master' or 'main', and the RELEASE_VERSION is not set."
+        exit 1
+    fi
 fi
 
 # The Environment name to Deploy cannot be 'master' or 'main'.
@@ -103,7 +106,15 @@ elif [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ "$BRANCH_NAME" == "master" || "$BRAN
         sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./../kube/values/$APP_NAME/prod/values-prod.yaml"
     fi
     cp -f -r "./../kube/values/$APP_NAME/prod/" "./"
-
+elif [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ ! -z "$RELEASE_VERSION" ]]; then
+    cd helm-chart-$APP_NAME-values-prod/
+    if [ "$IMAGE_TAG" != "" ]; then
+        sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./prod-$RELEASE_VERSION/values-prod-tag.yaml"
+    fi
+    if [ ! -z ${DEPLOYED_AT} ]; then
+        sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./../kube/values/$APP_NAME/prod-$RELEASE_VERSION/values-prod.yaml"
+    fi
+    cp -f -r "./../kube/values/$APP_NAME/prod-$RELEASE_VERSION/" "./"
 else
     cd helm-chart-$APP_NAME-values-staging/
     # Store the currentTag value before the deployment for rollout undo (just in case).
@@ -127,7 +138,7 @@ else
     if [[ $ROLLOUT == true ]]; then
         git commit -m "ROLLOUT UNDO in ${APP_NAME^^} - $IMAGE_TAG -> [${ENV_TO_DEPLOY^^}]"
     elif [[ $MANUAL == true ]] && [[ "$IMAGE_TAG" != "" ]]; then
-        git commit -m "MANUAL DEPLOYMENT in ${APP_NAME^^} - $IMAGE_TAG -> [${ENV_TO_DEPLOY^^}]"
+        git commit -m "MANUAL DEPLOYMENT in ${APP_NAME^^} - $IMAGE_TAG -> [${ENV_TO_DEPLOY^^}${RELEASE_VERSION:+-$RELEASE_VERSION}]"
     elif [[ $MANUAL == true ]]; then
         git commit -m "MANUAL DEPLOYMENT in ${APP_NAME^^} -> [${ENV_TO_DEPLOY^^}]"
     else
