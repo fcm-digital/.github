e7e96f7c1970789c3b90e3d50c2a6da85f41cc06
David Pacheco <David.Pacheco@fcm.digital>
2025-03-17 09:24:39 +0100

    update helm template for prod and staging

--- .github/actions/argocd-deployment/argocd-helm-template/entrypoint.sh
+++ .github/actions/argocd-deployment/argocd-helm-template/entrypoint.sh
@@ -16,14 +16,21 @@ for env in $(echo $ENVIRONMENTS | tr ',' '\n'); do
         done
     fi
 
-    VALUES_LOCAL_FILE=$(find "$VALUES_DIR/$env/" -iname "*.yaml*")
-    if [ ! -z "${VALUES_LOCAL_FILE}" ]; then
-        for value in $VALUES_LOCAL_FILE; do
-            VALUES+=$(echo "-f .$value ")
-        done
+    if [ $env != "prod" ]; then
+        VALUES_LOCAL_FILE=$(find "$VALUES_DIR/$env/" -iname "*.yaml*")
+        if [ ! -z "${VALUES_LOCAL_FILE}" ]; then
+            for value in $VALUES_LOCAL_FILE; do
+                VALUES+=$(echo "-f .$value ")
+            done
+        fi
     fi
 
     cd ./helm-chart-template
-    OUTPUT_FILE_NAME="$(echo $TEMPLATE_FILE | awk -F'.' '{print $(NF-1)}' )-$env.yaml"
-    helm template . -s templates/$TEMPLATE_FILE --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "$OUTPUT_FILE_NAME"
+
+    if [ ! -z "$TEMPLATE_FILE" ]; then
+        OUTPUT_FILE_NAME="$(echo $TEMPLATE_FILE | awk -F'.' '{print $(NF-1)}' )-$env.yaml"
+        helm template . -s templates/$TEMPLATE_FILE --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "$OUTPUT_FILE_NAME"
+    else
+        helm template . --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "$env.yaml"
+    fi
 done
