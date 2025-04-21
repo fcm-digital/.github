ac2fd86f35d297005dd6a7c4524547245d24d991
dapama <45515268+dapama@users.noreply.github.com>
2025-04-21 16:29:01 +0200

    Create argocd-helm-template action (#26)
    * create argocd-helm-template action
    
    * update helm template for prod and staging
    
    * Add get-env-to-deploy-on action
    
    * improve control error
    
    * fix typo
    
    * improve logic
    
    * test action (I)
    
    * test action (II)
    
    * test action (III)
    
    * test action (IV)
    
    * test action (V) - remove xargs
    
    * update messages
    
    * update messages (II)
    
    * improve helm template operation
    
    * improve helm template operation (II)

---
+++ .github/actions/argocd-deployment/argocd-helm-template/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+for env in $(echo $ENVIRONMENTS | tr ',' '\n'); do
+    if [ $env == "prod" ]; then
+        VALUES_DIR="./helm-chart-values/prod"
+    else
+        VALUES_DIR="./helm-chart-values/staging"
+    fi
+
+    VALUES_GLOBAL_FILE=$(find $VALUES_DIR/ -maxdepth 1 -iname "*.yaml*")
+    if [ ! -z "${VALUES_GLOBAL_FILE}" ]; then
+        for value in $VALUES_GLOBAL_FILE; do
+            VALUES+=$(echo "-f .$value ")
+        done
+    fi
+
+    if [ $env != "prod" ]; then
+        VALUES_LOCAL_FILE=$(find "$VALUES_DIR/$env/" -iname "*.yaml*")
+        if [ ! -z "${VALUES_LOCAL_FILE}" ]; then
+            for value in $VALUES_LOCAL_FILE; do
+                VALUES+=$(echo "-f .$value ")
+            done
+        fi
+    fi
+
+    cd ./helm-chart-template
+
+    if [ ! -z "$TEMPLATE_FILE" ]; then
+        OUTPUT_FILE_NAME="$(echo $TEMPLATE_FILE | awk -F'.' '{print $(NF-1)}' )-$env.yaml"
+        helm template . -s templates/$TEMPLATE_FILE --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "../$OUTPUT_FILE_NAME"
+    else
+        helm template . --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "../$env.yaml"
+    fi
+done
