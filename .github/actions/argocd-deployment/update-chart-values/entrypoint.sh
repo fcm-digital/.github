b2845866ed9d64ef80f09852802d6728c84d16f4
dapama <45515268+dapama@users.noreply.github.com>
2025-06-26 10:01:21 +0200

    Improve naming and commentaries (#30)
    * Improve commentary in update-chart-values action
    
    * Improve input descriptions in update-chart-values action

--- .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
+++ .github/actions/argocd-deployment/update-chart-values/entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
-
+# Initialize an empty string to store environments that have been synced
 synced_staging_envs=""
 
+# Function to add a new staging environment to the synced environments list
 add_synced_staging_envs() {
     local new_staging_env=$1
     if [ -z "$synced_staging_envs" ]; then
@@ -15,89 +16,93 @@ add_synced_staging_envs() {
 }
 
 
-# The branch name can only start with 'master' or 'main' if the branch is MASTER/MAIN ref.
+# Ensure the branch name is 'master' or 'main' when deploying to 'prod'
 if [[ "$ENV_TO_DEPLOY" == "prod" ]] && 
    [[ "$BRANCH_NAME" != "master" && "$BRANCH_NAME" != "main" ]]; then
 
+    # Check if RELEASE_VERSION is set
     if [[ -z "$RELEASE_VERSION" ]]; then
         echo "The Environment to Deploy cannot be 'prod' if the branches are not 'master' or 'main', and the RELEASE_VERSION is not set."
         exit 1
     fi
 fi
 
-# The Environment name to Deploy cannot be 'master' or 'main'.
+# Prevent deployment to 'master' or 'main' environment. There shouldn't be any env with these names.
 if [[ "$ENV_TO_DEPLOY" == "master" || "$ENV_TO_DEPLOY" == "main" ]]; then
     echo "The Environment to Deploy cannot be 'master' or 'main'."
     exit 1
 fi
 
-# The Environment name to Deploy cannot be 'ALL_ENV' if the branch is not 'master' or 'main'.
+# Prevent deployment to 'ALL_ENV' unless on 'master' or 'main' branch
 if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]] && [[ "$BRANCH_NAME" != "master" && "$BRANCH_NAME" != "main" ]]; then
     echo "It cannot be deployed in All Environments if the branch is not 'master' or 'main'."
     exit 1
 fi
 
-# Must be updated in each deploy if UPDATE_DEPLOYED_AT is true.
+# Update DEPLOYED_AT if required
 if [[ "$UPDATE_DEPLOYED_AT" = true ]]; then
     echo "Updating 'DEPLOYED_AT' env variable at runtime."
     DEPLOYED_AT=$(date -u +"%FT%TZ")
 fi
 
-# Iter over all values-*.yaml files in order to sync thier content with the local values.
+# Sync values files for all environments if deploying to 'ALL_ENV' on 'master' or 'main' branch
 if [[ "$ENV_TO_DEPLOY" == "ALL_ENV" ]] && [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
     cd helm-chart-$APP_NAME-values-staging/
     for env_path in $(ls -d -- ./staging/*/ 2>/dev/null); do
-        # Get the source of the 'currentTag' environment
+        # Determine the current environment and source file
         export CURRENT_ENV=$(basename "${env_path%/}")
         export CURRENT_SOURCE_FILE=$(echo "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/values-stg.yaml")
 
+        # Check if the source file exists
         if [[ -e $CURRENT_SOURCE_FILE ]]; then
             export CURRENT_IMAGE_TAG=$(cat "./staging/$CURRENT_ENV/values-stg-tag.yaml" | grep currentTag: | cut -d ':' -f 2 | sed 's/ //g')
             export CURRENT_IMAGE_TAG_ENV=$(cut -d '-' -f 1 <<< $( echo $CURRENT_IMAGE_TAG ))
 
-            # Check if the currentTag is an old 'master' image -> Then, sync the values.
+            # Sync values if the currentTag is an old 'master' image
             if [[ "$CURRENT_IMAGE_TAG_ENV" == "master" || "$CURRENT_IMAGE_TAG_ENV" == "main" || "$CURRENT_IMAGE_TAG_ENV" == "latest" || "$CURRENT_ENV" == "sandbox" ]]; then
 
-                # Skip the deployment on sandbox if the DEPLOY_ON_SANDBOX is false.
+                # Skip sandbox deployment if not required
                 if [[ $DEPLOY_ON_SANDBOX == false && "$CURRENT_ENV" == "sandbox" ]]; then
                     continue
                 fi
 
-                # Update the currentTag to the new image tag if it is not empty.
+                # Update the currentTag if IMAGE_TAG is set
                 if [ "$IMAGE_TAG" != "" ]; then
                     sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$CURRENT_ENV/values-stg-tag.yaml"
                 fi
 
-                # Update the DEPLOYED_AT env variable if it is not empty.
+                # Update DEPLOYED_AT if set
                 if [ ! -z ${DEPLOYED_AT+x} ]; then
                     sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" $CURRENT_SOURCE_FILE
                 fi
 
-                # Add the current environment to the synced_staging_envs variable if SYNCED_ENVS_AS_OUTPUTS is true.
+                # Add environment to synced list if required
                 if [[ $SYNCED_ENVS_AS_OUTPUTS == true ]]; then
                     add_synced_staging_envs $CURRENT_ENV
                 fi
 
-                # Sync the values of the current environment from the local code repository to the helm-chart-$APP_NAME-values/staging repository.
+                # Sync values from local to helm-chart repository
                 cp -f -r "./../kube/values/$APP_NAME/staging/$CURRENT_ENV/" "./staging/"
             fi
         else
             echo "$CURRENT_ENV not found in local code repository, but existing in helm-chart-$APP_NAME-values/staging repository."
         fi
     done
-    # The values-stg.yaml will always be synced when a Pull Request is closed.
+    # Always sync values-stg.yaml when a Pull Request is closed
     cp -f "./../kube/values/$APP_NAME/staging/values-stg.yaml" "./staging/values-stg.yaml"
     if [ ! -z ${synced_staging_envs+x} ]; then
         echo "synced_staging_envs=$( echo $synced_staging_envs )" >> $GITHUB_OUTPUT
     fi
 
-elif [[ "$ENV_TO_DEPLOY" == "NO_SYNC" ]] && [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
+# Sync common staging values if deploying to 'ONLY_GLOBAL_STG_VALUES' on 'master' or 'main' branch
+elif [[ "$ENV_TO_DEPLOY" == "ONLY_COMMON_STG_VALUES" ]] && [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
     cd helm-chart-$APP_NAME-values-staging/
     cp -f "./../kube/values/$APP_NAME/staging/values-stg.yaml" "./staging/values-stg.yaml"
 
+# Sync prod values if deploying to 'prod' on 'master' or 'main' branch
 elif [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "main" ]]; then
     cd helm-chart-$APP_NAME-values-prod/
-    # Store the currentTag value before the deployment for rollout undo (just in case).
+    # Store the currentTag for potential rollback
     echo "old_image_tag=$(cat "./prod/values-prod-tag.yaml" | grep currentTag: | cut -d ':' -f 2 | sed 's/ //g')" >> $GITHUB_OUTPUT
     if [ "$IMAGE_TAG" != "" ]; then
         sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./prod/values-prod-tag.yaml"
@@ -106,6 +111,8 @@ elif [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ "$BRANCH_NAME" == "master" || "$BRAN
         sed -i "{s/DEPLOYED_AT:.*/DEPLOYED_AT: $DEPLOYED_AT/;}" "./../kube/values/$APP_NAME/prod/values-prod.yaml"
     fi
     cp -f -r "./../kube/values/$APP_NAME/prod/" "./"
+
+# Sync prod values if deploying to 'prod' for a specific 'release version'
 elif [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ ! -z "$RELEASE_VERSION" ]]; then
     cd helm-chart-$APP_NAME-values-prod/
     if [ "$IMAGE_TAG" != "" ]; then
@@ -117,7 +124,7 @@ elif [[ "$ENV_TO_DEPLOY" == "prod" ]] && [[ ! -z "$RELEASE_VERSION" ]]; then
     cp -f -r "./../kube/values/$APP_NAME/prod-$RELEASE_VERSION/" "./"
 else
     cd helm-chart-$APP_NAME-values-staging/
-    # Store the currentTag value before the deployment for rollout undo (just in case).
+    # Store the currentTag for potential rollback
     echo "old_image_tag=$(cat "./staging/$ENV_TO_DEPLOY/values-stg-tag.yaml" | grep currentTag: | cut -d ':' -f 2 | sed 's/ //g')" >> $GITHUB_OUTPUT
     if [ "$IMAGE_TAG" != "" ]; then
         sed -i "{s/currentTag:.*/currentTag: $IMAGE_TAG/;}" "./staging/$ENV_TO_DEPLOY/values-stg-tag.yaml"
@@ -128,6 +135,7 @@ else
     cp -f -r "./../kube/values/$APP_NAME/staging/$ENV_TO_DEPLOY/" "./staging/"
 fi
 
+# Check for changes and commit if there are any
 if [ -z "$(git diff --exit-code)" ]; then
     echo "No changes in the working directory."
 else
@@ -135,6 +143,7 @@ else
     git config user.email github-actions@github.com
     git pull
     git add .
+    # Commit changes with appropriate message
     if [[ $ROLLOUT == true ]]; then
         git commit -m "ROLLOUT UNDO in ${APP_NAME^^} - $IMAGE_TAG -> [${ENV_TO_DEPLOY^^}]"
     elif [[ $MANUAL == true ]] && [[ "$IMAGE_TAG" != "" ]]; then
