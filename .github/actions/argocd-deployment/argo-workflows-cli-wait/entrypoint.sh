e9d302f95fe5e6933ef0647ca1727712daf7ae8f
David Pacheco <David.Pacheco@fcm.digital>
2025-09-09 14:57:31 +0200

    feat: add labels input for argo-workflows-wait action

--- .github/actions/argocd-deployment/argo-workflows-cli-wait/entrypoint.sh
+++ .github/actions/argocd-deployment/argo-workflows-cli-wait/entrypoint.sh
@@ -2,12 +2,17 @@
 
 set -euo pipefail
 
-echo "Waiting for Argo Workflow to complete"
+if [[ -z $LABELS ]]; then
+    LABELS=""
+else
+    LABELS="-l $LABELS"
+fi
 
-argo wait @latest -n $ENVIRONMENTS
+echo "Waiting for Argo Workflow to complete"
+argo wait @latest -n $ENVIRONMENTS $LABELS
 if [[ $? -eq 0 ]]; then
     echo "Argo Workflow submitted successfully"
-    argo get @latest -n $ENVIRONMENTS
+    argo get @latest -n $ENVIRONMENTS $LABELS
 else
     echo "Argo Workflow failed"
     exit 1
