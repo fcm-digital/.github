0b01b2f3f1317f792d22c25075601da78ee98bcf
David Pacheco <David.Pacheco@fcm.digital>
2025-09-30 13:08:14 +0200

    feat: allow multiple resources comma-separated

--- .github/actions/argocd-deployment/argocd-cli-sync-app/sync_app_entrypoint.sh
+++ .github/actions/argocd-deployment/argocd-cli-sync-app/sync_app_entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 argocd_app_sync () {
-    argocd app sync $ARGOCD_FULL_APP_NAME ${RESOURCES:+--resource "$RESOURCES"} \
+    echo "argocd app sync $ARGOCD_FULL_APP_NAME $resource_args \
         --server $ARGOCD_URL \
         --auth-token $ARGOCD_AUTH_TOKEN \
         --prune \
         --retry-limit 2 \
         --retry-backoff-duration 5s \
-        --retry-backoff-factor 2
+        --retry-backoff-factor 2"
 }
 
 argocd_app_wait () {
@@ -26,6 +26,14 @@ fi
 
 ITER=1
 
+resource_args=""
+if [[ -n "$RESOURCES" ]]; then
+    IFS=',' read -ra RESOURCE_ARRAY <<< "$RESOURCES"
+    for resource in "${RESOURCE_ARRAY[@]}"; do
+        resource_args+="--resource $resource "
+    done
+fi
+
 until argocd_app_sync </dev/null
 do
     if [ $ITER -eq 3 ]; then
@@ -38,12 +46,12 @@ done
 
 ITER=1
 
-until argocd_app_wait </dev/null
-do
-    if [ $ITER -eq 3 ]; then
-        exit 1
-    fi
+# until argocd_app_wait </dev/null
+# do
+#     if [ $ITER -eq 3 ]; then
+#         exit 1
+#     fi
 
-    sleep $((10 * $ITER))s
-    ITER=$(($ITER + 1))
-done
+#     sleep $((10 * $ITER))s
+#     ITER=$(($ITER + 1))
+# done
