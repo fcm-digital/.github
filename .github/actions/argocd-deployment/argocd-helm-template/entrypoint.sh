d2827c24865e4646625593340d2a99982b898717
dapama <45515268+dapama@users.noreply.github.com>
2026-01-20 09:01:54 +0100

    [v4.0.0] Deprecated GH and GCP Auth with Token (#39)
    * refactor: make CLI version inputs required and improve version handling in GCP actions
    
    * refactor: replace GitHub token with GitHub App authentication in ArgoCD checkout actions
    
    * fix: update repositories parameter name
    
    * fix: use output token from previous github app auth
    
    * fix: add missing input variable for argocd-helm-template action
    
    * feature: allow multiple template files for argocd-helm-template action
    
    * feature: add file-name as output
    
    * revert latest commit
    
    * refactor: replace GCP JSON auth with Workload Identity Federation for container registry access
    
    * ci: add permissions for github-token to use WIF authentication
    
    * ci: add global permissions for reusable workflow
    
    * Revert "ci: add global permissions for reusable workflow"
    
    This reverts commit 688d40aa32d6dfab8530fe92ddba90fa999ba017.

--- .github/actions/argocd-deployment/argocd-helm-template/entrypoint.sh
+++ .github/actions/argocd-deployment/argocd-helm-template/entrypoint.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 for env in $(echo $ENVIRONMENTS | tr ',' '\n'); do
     if [ $env == "prod" ]; then
-        VALUES_DIR="./helm-chart-values/prod"
+        VALUES_DIR="./$HELM_CHART_VALUES_PATH/prod"
     else
-        VALUES_DIR="./helm-chart-values/staging"
+        VALUES_DIR="./$HELM_CHART_VALUES_PATH/staging"
     fi
 
     VALUES_GLOBAL_FILE=$(find $VALUES_DIR/ -maxdepth 1 -iname "*.yaml*")
@@ -25,12 +25,16 @@ for env in $(echo $ENVIRONMENTS | tr ',' '\n'); do
         fi
     fi
 
-    cd ./helm-chart-template
+    cd ./$HELM_CHART_TEMPLATE_PATH
 
-    if [ ! -z "$TEMPLATE_FILE" ]; then
-        OUTPUT_FILE_NAME="$(echo $TEMPLATE_FILE | awk -F'.' '{print $(NF-1)}' )-$env.yaml"
-        helm template . -s templates/$TEMPLATE_FILE --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "../$OUTPUT_FILE_NAME"
+    OUTPUT_FILE_NAME="$APP_NAME-$env.yaml"
+    if [ ! -z "$TEMPLATE_FILES" ]; then
+        TEMPLATE_ARGS=""
+        for template in $(echo $TEMPLATE_FILES | tr ',' '\n'); do
+            TEMPLATE_ARGS+=" -s templates/$template"
+        done
+        helm template . $TEMPLATE_ARGS --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "../$OUTPUT_FILE_NAME"
     else
-        helm template . --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "../$env.yaml"
+        helm template . --name-template=$APP_NAME --namespace=$env $VALUES --set currentTag=$IMAGE_TAG > "../$OUTPUT_FILE_NAME"
     fi
 done
