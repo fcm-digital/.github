9266b71cd7abc5fb27fb346308362f9129938b48
David Pacheco <David.Pacheco@fcm.digital>
2026-02-23 09:01:09 +0100

    feat: add argo submit command to include watch and log arguments

--- .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
+++ .github/actions/argocd-deployment/argo-workflows-cli-submit/entrypoint.sh
@@ -8,16 +8,15 @@ for env in $(echo $ENVIRONMENTS | tr ',' '\n'); do
     ARGO_WORKFLOW_FILE="$WORKFLOWS_PATH/$WORKFLOW_NAME-$env.yaml"
     if [[ -f $ARGO_WORKFLOW_FILE ]]; then
         echo "Submitting Argo Workflow file $ARGO_WORKFLOW_FILE"
-        argo submit $ARGO_WORKFLOW_FILE
-        if [[ $? -eq 0 ]]; then
-            argo wait @latest -n $env
-            if [[ $? -eq 0 ]]; then
-                echo "Argo Workflow $ARGO_WORKFLOW_FILE submitted successfully"
-                argo get @latest -n $env
-            fi
+        if argo submit --watch --log $ARGO_WORKFLOW_FILE -n $env; then
+            echo "Argo Workflow $ARGO_WORKFLOW_FILE completed successfully"
+            argo get @latest -n $env
+        else
+            echo "ERROR: Argo Workflow watch failed for $ARGO_WORKFLOW_FILE"
+            exit 1
         fi
     else
-        echo "File $ARGO_WORKFLOW_FILE not found"
+        echo "ERROR: Argo Workflow file $ARGO_WORKFLOW_FILE not found"
         exit 1
     fi
 done
